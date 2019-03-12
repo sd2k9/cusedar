sd2k9/cusedar

commit 459a78b25601f1c907ddd8b13dab115c0af1f4c5

Replace fid with cusedar, update descriptions

chrome/content/helpers.js
@@ -21,14 +21,14 @@
   See README.md for more details.
 */
 
-var fidHelpers = {
+var cusedarHelpers = {
 
 onLoad: function() {
     try {
         let prefs = Components.classes['@mozilla.org/preferences-service;1'].
             getService(Components.interfaces.nsIPrefBranch);
 
-        this.pref = 'extensions.fid.welcome.version';
+        this.pref = 'extensions.cusedar.welcome.version';
         this.version = 1;
 
         if (this.willShowNotify(prefs)) {
@@ -55,28 +55,28 @@ showNotify: function() {
 
     let bundle = Components.classes['@mozilla.org/intl/stringbundle;1'].
         getService(Components.interfaces.nsIStringBundleService).
-        createBundle('chrome://fid/locale/rules.properties');
+        createBundle('chrome://cusedar/locale/rules.properties');
 
-    let notifyText = bundle.GetStringFromName('fidWelcome');
+    let notifyText = bundle.GetStringFromName('cusedarWelcome');
 
     var buttons = [
         {
-            label: bundle.GetStringFromName('fidWelcomeAction'),
+            label: bundle.GetStringFromName('cusedarWelcomeAction'),
             accessKey: null,
             popup: null,
             callback: function(aNotificationBar, aButton) {
-                window.open('chrome://fid/content/options.xul', '', 'chrome,centerscreen,modal,resizable');
+                window.open('chrome://cusedar/content/options.xul', '', 'chrome,centerscreen,modal,resizable');
                 return true;
             }
         }
     ];
 
-    var box = notifyBox.appendNotification(notifyText, "about-fid",
+    var box = notifyBox.appendNotification(notifyText, "about-cusedar",
         null, notifyBox.PRIORITY_INFO_LOW, buttons);
 
     box.persistence = 4;
 },
 
-} // fidHelpers
+} // cusedarHelpers
 
-window.addEventListener('load', function() {fidHelpers.onLoad();}, false);
+window.addEventListener('load', function() cusedarHelpers.onLoad();}, false);

chrome.manifest
@@ -1,9 +1,9 @@
-content fid             chrome/content/
-locale  fid en-US       chrome/locale/en-US/
-locale  fid ru-RU       chrome/locale/ru-RU/
-locale  fid fr-FR       chrome/locale/fr-FR/
-locale  fid de-DE       chrome/locale/de-DE/
-skin    fid classic/1.0 chrome/skin/classic/
+content cusedar             chrome/content/
+locale  cusedar en-US       chrome/locale/en-US/
+locale  cusedar ru-RU       chrome/locale/ru-RU/
+locale  cusedar fr-FR       chrome/locale/fr-FR/
+locale  cusedar de-DE       chrome/locale/de-DE/
+skin    cusedar classic/1.0 chrome/skin/classic/
 
-overlay	chrome://messenger/content/messengercompose/messengercompose.xul chrome://fid/content/compose-overlay.xul
-overlay	chrome://messenger/content/messenger.xul                         chrome://fid/content/messenger-overlay.xul
+overlay	chrome://messenger/content/messengercompose/messengercompose.xul chrome://cusedar/content/compose-overlay.xul
+overlay	chrome://messenger/content/messenger.xul                         chrome://cusedar/content/messenger-overlay.xul

chrome/content/compose-overlay.xul
@@ -28,20 +28,20 @@
 //-->
 
 <!DOCTYPE window [
-  <!ENTITY % dtd1 SYSTEM "chrome://fid/locale/branding.dtd"> %dtd1;
-  <!ENTITY % dtd2 SYSTEM "chrome://fid/locale/overlay.dtd"> %dtd2;
+  <!ENTITY % dtd1 SYSTEM "chrome://cusedar/locale/branding.dtd"> %dtd1;
+  <!ENTITY % dtd2 SYSTEM "chrome://cusedar/locale/overlay.dtd"> %dtd2;
 ]>
 
 <overlay xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul">
 
-<script type="application/javascript" src="chrome://fid/content/rules.js"/>
-<script type="application/javascript" src="chrome://fid/content/identity.js"/>
-<script type="application/javascript" src="chrome://fid/content/reply_as_original_recipient.js" />
+<script type="application/javascript" src="chrome://cusedar/content/rules.js"/>
+<script type="application/javascript" src="chrome://cusedar/content/identity.js"/>
+<script type="application/javascript" src="chrome://cusedar/content/reply_as_original_recipient.js" />
 
 <menupopup id="optionsMenuPopup">
     <menuseparator/>
     <menuitem label="&overlay.menuitem;" accesskey="&overlay.accesskey;"
-        oncommand="fidIdentity.showOptions();"/>
+        oncommand="cusedarIdentity.showOptions();"/>
 </menupopup>
 
 </overlay>

chrome/content/identity.js
@@ -22,7 +22,7 @@
   See README.md for more details.
 */
 
-var fidIdentity = {
+var cusedarIdentity = {
 
 onLoad: function() {
 
@@ -32,48 +32,48 @@ onLoad: function() {
     try {
 	let prefs = Components.classes['@mozilla.org/preferences-service;1'].
             getService(Components.interfaces.nsIPrefBranch);
-	pref_debug  = prefs.getBoolPref('extensions.fid.debug.console');
+	pref_debug  = prefs.getBoolPref('extensions.cusedar.debug.console');
 	if (pref_debug)  /* We want to print, so we fetch the console */
 	    this.console = (Components.utils.import("resource://gre/modules/Console.jsm", {})).console;
     } catch (ex) {Components.utils.reportError(ex);}
 
     try {
-        this.ro = new fidRules(pref_debug);  /* C'tor with debug console */
+        this.ro = new cusedarRules(pref_debug);  /* C'tor with debug console */
         this.embedIntoCompose();
 	this.raor = null;          /* Reference to raor */
 	if ('ReplyAsOriginalRecipient' in window)
 	    this.raor = window.ReplyAsOriginalRecipient;
 	else /* something went wrong, but just report it */
 	    if (this.console)
-		this.console.error("ERROR fid: Did not found ReplyAsOriginalRecipient in window object!");
+		this.console.error("ERROR cusedar: Did not found ReplyAsOriginalRecipient in window object!");
     } catch (ex) {Components.utils.reportError(ex);}
 },
 
 showOptions: function() {
     var res = {};
-    window.openDialog('chrome://fid/content/options.xul', '', 'chrome,centerscreen,modal,resizable', res);
+    window.openDialog('chrome://cusedar/content/options.xul', '', 'chrome,centerscreen,modal,resizable', res);
 },
 
 embedIntoCompose: function() {
-    var fid_SendMessageWithCheck = SendMessageWithCheck;
-    var fid_SendMessage = SendMessage;
-    var fid_SendMessageLater = SendMessageLater;
-    var fid_GenericSendMessage = GenericSendMessage;
+    var cusedar_SendMessageWithCheck = SendMessageWithCheck;
+    var cusedar_SendMessage = SendMessage;
+    var cusedar_SendMessageLater = SendMessageLater;
+    var cusedar_GenericSendMessage = GenericSendMessage;
 
     window.SendMessageLater = function () {
-        fidIdentity.checkAndSend(fid_SendMessageLater);
+        cusedarIdentity.checkAndSend(cusedar_SendMessageLater);
     };
 
     window.SendMessageWithCheck = function () {
-        fidIdentity.checkAndSend(fid_SendMessageWithCheck);
+        cusedarIdentity.checkAndSend(cusedar_SendMessageWithCheck);
     };
 
     window.SendMessage = function () {
-        fidIdentity.checkAndSend(fid_SendMessage);
+        cusedarIdentity.checkAndSend(cusedar_SendMessage);
     };
 
     window.GenericSendMessage = function(msgType) {
-        fidIdentity.genericSend(msgType, fid_GenericSendMessage);
+        cusedarIdentity.genericSend(msgType, cusedar_GenericSendMessage);
     }
 },
 
@@ -86,7 +86,7 @@ checkAndSend: function(aCallback) {
             if (!this.checkRules(true))
 		return;
 	} else if (this.console) { /* Debug output when requested */
-	    this.console.log("DEBUG fid: No action, because cusedar/raor did ran (1)");
+	    this.console.log("DEBUG cusedar: No action, because cusedar/raor did ran (1)");
 	}
     } catch(ex) {Components.utils.reportError(ex);}
 
@@ -128,8 +128,8 @@ checkRules: function(willSend) {
     try {
 	let prefs = Components.classes['@mozilla.org/preferences-service;1'].
             getService(Components.interfaces.nsIPrefBranch);
-	pref_sendername = prefs.getCharPref('extensions.fid.reply.sendername');
-	pref_addrbook = prefs.getBoolPref('extensions.fid.addressbook');
+	pref_sendername = prefs.getCharPref('extensions.cusedar.reply.sendername');
+	pref_addrbook = prefs.getBoolPref('extensions.cusedar.addressbook');
     } catch (ex) {Components.utils.reportError(ex);}
 
     let abfrom = null;   /* Fill with addressbook from when found */
@@ -139,13 +139,13 @@ checkRules: function(willSend) {
 	/* When enabled, first check the adress book */
 	abfrom = this.ro.matchAddrbook(recpts);
 	if (this.console) {
-	    this.console.log("DEBUG fid: addrbook lookup returned = ", abfrom);
+	    this.console.log("DEBUG cusedar: addrbook lookup returned = ", abfrom);
 	}
-    } /* if (prefs.getBoolPref('extensions.fid.addressbook')) */
+    } /* if (prefs.getBoolPref('extensions.cusedar.addressbook')) */
 
     if (abfrom != null) {
 	if (this.console) {
-	    this.console.log("DEBUG fid: Use addrbook lookup to update sender");
+	    this.console.log("DEBUG cusedar: Use addrbook lookup to update sender");
 	}
 	try {
 	    let sendername = null;
@@ -154,7 +154,7 @@ checkRules: function(willSend) {
 	    if ( (abfrom.indexOf("<") != -1) && (abfrom.indexOf(">") != -1) &&
 	         (abfrom.indexOf("<") < abfrom.indexOf(">")) ) {
 		if (this.console) {
-		    this.console.log("DEBUG fid: Do not set sender, assuming it's part of Custom3");
+		    this.console.log("DEBUG cusedar: Do not set sender, assuming it's part of Custom3");
 		}
 	    } else if (pref_sendername.length > 0) /* Sender name from configuration */
 		sendername = pref_sendername;
@@ -170,7 +170,7 @@ checkRules: function(willSend) {
 
 	/* Debug Console Output */
 	if (this.console)
-	    this.console.log("DEBUG fid: idx = ", idx);
+	    this.console.log("DEBUG cusedar: idx = ", idx);
 
 	if (idx != -1) {
 	    /* We got a rules match! */
@@ -179,8 +179,8 @@ checkRules: function(willSend) {
             let useAttr = cmb.selectedItem.hasAttribute('identitykey');
             let from = useAttr ? cmb.selectedItem.getAttribute('identitykey') : cmb.value;
 	    if (this.console) {
-		this.console.log("DEBUG fid: key = ", key);
-		this.console.log("DEBUG fid: from = ", from);
+		this.console.log("DEBUG cusedar: key = ", key);
+		this.console.log("DEBUG cusedar: from = ", from);
 	    }
             let fixError = true;
 
@@ -221,11 +221,11 @@ checkRules: function(willSend) {
 			cmb.value = MailServices.headerParser.makeMailboxObject(
                             identity.fullName, identity.email).toString();
 			if (this.console)
-			    this.console.log("DEBUG fid: Update cmb.value = makeMailboxObject = ", cmb.value);
+			    this.console.log("DEBUG cusedar: Update cmb.value = makeMailboxObject = ", cmb.value);
                     } else {
 			cmb.value = key;
 			if (this.console)
-			    this.console.log("DEBUG fid: Update cmb.value = key = ", cmb.value);
+			    this.console.log("DEBUG cusedar: Update cmb.value = key = ", cmb.value);
 		    }
 
                     LoadIdentity(false); // thanks to Bruce Jolliffe
@@ -240,7 +240,7 @@ checkCC: function() {
     let prefs = Components.classes['@mozilla.org/preferences-service;1'].
         getService(Components.interfaces.nsIPrefBranch);
 
-    if (!prefs.getBoolPref('extensions.fid.check.cc'))
+    if (!prefs.getBoolPref('extensions.cusedar.check.cc'))
         return true;
 
     let i = 0;
@@ -281,7 +281,7 @@ checkCC: function() {
         check);
 
     if (check.value) {
-        prefs.setBoolPref('extensions.fid.check.cc', false);
+        prefs.setBoolPref('extensions.cusedar.check.cc', false);
     }
 
    return ask !== 1;
@@ -294,7 +294,7 @@ genericSend: function(aType, aCallback) {
             if (aType == nsIMsgCompDeliverMode.SaveAsDraft && !this.checkDraft())
 		return;
 	} else if (this.console) { /* Debug output when requested */
-	    this.console.log("DEBUG fid: No action, because cusedar/raor did ran (2)");
+	    this.console.log("DEBUG cusedar: No action, because cusedar/raor did ran (2)");
 	}
     } catch(ex) {Components.utils.reportError(ex);}
 
@@ -306,14 +306,14 @@ checkDraft: function() {
 	/* Only check for replacement when raor did not already replaced the sender */
 	let prefs = Components.classes['@mozilla.org/preferences-service;1'].
             getService(Components.interfaces.nsIPrefBranch);
-	return prefs.getBoolPref('extensions.fid.check.draft') ?
+	return prefs.getBoolPref('extensions.cusedar.check.draft') ?
             this.checkRules(false) : true;
     } else if (this.console) { /* Debug output when requested */
-	this.console.log("DEBUG fid: No action, because cusedar/raor did ran (3)");
+	this.console.log("DEBUG cusedar: No action, because cusedar/raor did ran (3)");
 	return true;
     }
 },
 
-} // fidOptions
+} // cusedarOptions
 
-window.addEventListener('load', function() {fidIdentity.onLoad();}, false);
+window.addEventListener('load', function() {cusedarIdentity.onLoad();}, false);

chrome/content/messenger-overlay.xul
@@ -25,12 +25,12 @@
 //-->
 
 <!DOCTYPE window [
-  <!ENTITY % dtd1 SYSTEM "chrome://fid/locale/branding.dtd"> %dtd1;
-  <!ENTITY % dtd2 SYSTEM "chrome://fid/locale/overlay.dtd"> %dtd2;
+  <!ENTITY % dtd1 SYSTEM "chrome://cusedar/locale/branding.dtd"> %dtd1;
+  <!ENTITY % dtd2 SYSTEM "chrome://cusedar/locale/overlay.dtd"> %dtd2;
 ]>
 
 <overlay xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul">
 
-<script type="application/javascript" src="chrome://fid/content/helpers.js"/>
+<script type="application/javascript" src="chrome://cusedar/content/helpers.js"/>
 
 </overlay>

chrome/content/options.js
@@ -22,11 +22,11 @@
   See README.md for more details.
 */
 
-var fidOptions = {
+var cusedarOptions = {
 
 onLoad: function() {
     try {
-        this.ro = new fidRules();
+        this.ro = new cusedarRules();
         this.lb = document.getElementById('id-rules');
         this.idx = -1;
         this.fillIds();
@@ -171,8 +171,8 @@ doAccept: function() {
         while(wm.hasMoreElements()) {
             let win = wm.getNext();
 
-            if ('fidIdentity' in win)
-                win.fidIdentity.ro.load();
+            if ('cusedarIdentity' in win)
+                win.cusedarIdentity.ro.load();
         }
     } catch(ex) {Components.utils.reportError(ex);}
 
@@ -232,16 +232,16 @@ fillPrefs: function() {
     let prefs = Components.classes['@mozilla.org/preferences-service;1'].
        getService(Components.interfaces.nsIPrefBranch);
 
-    document.getElementById('id-checkcc').checked        = prefs.getBoolPref('extensions.fid.check.cc');
-    document.getElementById('id-checkdraft').checked     = prefs.getBoolPref('extensions.fid.check.draft');
-    document.getElementById('id-showregexp').checked     = prefs.getBoolPref('extensions.fid.show.regexp');
-    document.getElementById('id-addr').checked           = prefs.getBoolPref('extensions.fid.addressbook');
-    document.getElementById('id-debug-console').checked  = prefs.getBoolPref('extensions.fid.debug.console');
+    document.getElementById('id-checkcc').checked        = prefs.getBoolPref('extensions.cusedar.check.cc');
+    document.getElementById('id-checkdraft').checked     = prefs.getBoolPref('extensions.cusedar.check.draft');
+    document.getElementById('id-showregexp').checked     = prefs.getBoolPref('extensions.cusedar.show.regexp');
+    document.getElementById('id-addr').checked           = prefs.getBoolPref('extensions.cusedar.addressbook');
+    document.getElementById('id-debug-console').checked  = prefs.getBoolPref('extensions.cusedar.debug.console');
 
-    document.getElementById('id-reply-enable').checked   = prefs.getBoolPref('extensions.fid.reply.enable');
-    document.getElementById('id-reply-checkcc').checked  = prefs.getBoolPref('extensions.fid.reply.checkcc');
-    document.getElementById('id-reply-regexp').value     = prefs.getCharPref('extensions.fid.reply.regexp');
-    document.getElementById('id-reply-sendername').value = prefs.getCharPref('extensions.fid.reply.sendername');
+    document.getElementById('id-reply-enable').checked   = prefs.getBoolPref('extensions.cusedar.reply.enable');
+    document.getElementById('id-reply-checkcc').checked  = prefs.getBoolPref('extensions.cusedar.reply.checkcc');
+    document.getElementById('id-reply-regexp').value     = prefs.getCharPref('extensions.cusedar.reply.regexp');
+    document.getElementById('id-reply-sendername').value = prefs.getCharPref('extensions.cusedar.reply.sendername');
 
     this.showOptFields();
 },
@@ -250,16 +250,16 @@ savePrefs: function() {
     let prefs = Components.classes['@mozilla.org/preferences-service;1'].
        getService(Components.interfaces.nsIPrefBranch);
 
-    prefs.setBoolPref('extensions.fid.check.cc',      document.getElementById('id-checkcc').checked);
-    prefs.setBoolPref('extensions.fid.check.draft',   document.getElementById('id-checkdraft').checked);
-    prefs.setBoolPref('extensions.fid.show.regexp',   document.getElementById('id-showregexp').checked);
-    prefs.setBoolPref('extensions.fid.addressbook',   document.getElementById('id-addr').checked);
-    prefs.setBoolPref('extensions.fid.debug.console', document.getElementById('id-debug-console').checked);
+    prefs.setBoolPref('extensions.cusedar.check.cc',      document.getElementById('id-checkcc').checked);
+    prefs.setBoolPref('extensions.cusedar.check.draft',   document.getElementById('id-checkdraft').checked);
+    prefs.setBoolPref('extensions.cusedar.show.regexp',   document.getElementById('id-showregexp').checked);
+    prefs.setBoolPref('extensions.cusedar.addressbook',   document.getElementById('id-addr').checked);
+    prefs.setBoolPref('extensions.cusedar.debug.console', document.getElementById('id-debug-console').checked);
 
-    prefs.setBoolPref('extensions.fid.reply.enable',  document.getElementById('id-reply-enable').checked);
-    prefs.setBoolPref('extensions.fid.reply.checkcc', document.getElementById('id-reply-checkcc').checked);
-    prefs.setCharPref('extensions.fid.reply.regexp',  document.getElementById('id-reply-regexp').value.trim());
-    prefs.setCharPref('extensions.fid.reply.sendername', document.getElementById('id-reply-sendername').value.trim());
+    prefs.setBoolPref('extensions.cusedar.reply.enable',  document.getElementById('id-reply-enable').checked);
+    prefs.setBoolPref('extensions.cusedar.reply.checkcc', document.getElementById('id-reply-checkcc').checked);
+    prefs.setCharPref('extensions.cusedar.reply.regexp',  document.getElementById('id-reply-regexp').value.trim());
+    prefs.setCharPref('extensions.cusedar.reply.sendername', document.getElementById('id-reply-sendername').value.trim());
 },
 
 openURL: function(aURL) {
@@ -277,6 +277,6 @@ showOptFields: function() {
     document.getElementById('id-reply-options').hidden = hide_reply_opts;
 }
 
-} // fidOptions
+} // cusedarOptions
 
-window.addEventListener('load', function() {fidOptions.onLoad();}, false);
+window.addEventListener('load', function() {cusedarOptions.onLoad();}, false);

chrome/content/options.xul
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 
 <?xml-stylesheet href="chrome://global/skin/" type="text/css"?>
-<?xml-stylesheet href="chrome://fid/skin/options.css" type="text/css"?>
+<?xml-stylesheet href="chrome://cusedar/skin/options.css" type="text/css"?>
 
 <!--
   Parts of this software stem from Flexible Identity.
@@ -27,8 +27,8 @@
 //-->
 
 <!DOCTYPE window [
-  <!ENTITY % dtd1 SYSTEM "chrome://fid/locale/branding.dtd"> %dtd1;
-  <!ENTITY % dtd2 SYSTEM "chrome://fid/locale/options.dtd"> %dtd2;
+  <!ENTITY % dtd1 SYSTEM "chrome://cusedar/locale/branding.dtd"> %dtd1;
+  <!ENTITY % dtd2 SYSTEM "chrome://cusedar/locale/options.dtd"> %dtd2;
   <!ENTITY % dtd3 SYSTEM "chrome://global/locale/dialogOverlay.dtd"> %dtd3;
 ]>
 
@@ -36,10 +36,10 @@
     title="&options.title;" id="id-window" persist="screenX screenY width height sizemode"
 >
 
-<script type="application/javascript" src="chrome://fid/content/rules.js"/>
-<script type="application/javascript" src="chrome://fid/content/options.js"/>
+<script type="application/javascript" src="chrome://cusedar/content/rules.js"/>
+<script type="application/javascript" src="chrome://cusedar/content/options.js"/>
 
-<tabbox id="id-tabs" flex="1" onselect="fidOptions.doTabs();">
+<tabbox id="id-tabs" flex="1" onselect="cusedarOptions.doTabs();">
     <tabs>
         <tab label="&options.rules;"/>
         <tab label="&options.other;"/>
@@ -58,7 +58,7 @@
                     <spacer class="button-gap"/>
 
                     <row flex="1">
-                        <listbox flex="1" id="id-rules" onselect="fidOptions.doSelect();"/>
+                        <listbox flex="1" id="id-rules" onselect="cusedarOptions.doSelect();"/>
 
                         <grid flex="1">
                             <columns>
@@ -68,7 +68,7 @@
                             <rows>
                                 <row align="center">
                                     <label value="&options.rule;"/>
-                                    <textbox id="id-rule" oninput="fidOptions.doUpdateCaption();"/>
+                                    <textbox id="id-rule" oninput="cusedarOptions.doUpdateCaption();"/>
                                 </row>
                                 <row flex="1">
                                     <label value="&options.scope;"/>
@@ -112,9 +112,9 @@
                 <groupbox>
                     <caption label="&options.other.advanced;"/>
                     <checkbox id="id-showregexp" label="&options.show.regexp;"
-                        oncommand="fidOptions.showOptFields();"/>
+                        oncommand="cusedarOptions.showOptFields();"/>
                     <label class="text-link indent" value="&options.show.regexp.description;"
-                        onclick="fidOptions.openURL('&options.show.regexp.link;');"/>
+                        onclick="cusedarOptions.openURL('&options.show.regexp.link;');"/>
                     <checkbox id="id-addr" label="&options.addressbook;"/>
                     <label class="indent" value="&options.addressbook.description;"/>
                     <checkbox id="id-debug-console" label="&options.debug.console;"/>
@@ -127,7 +127,7 @@
           <vbox flex="1" pack="start">
             <caption label="&options.reply.description;"/>
             <checkbox id="id-reply-enable" label="&options.reply.enable;"
-		      oncommand="fidOptions.showOptFields();"/>
+		      oncommand="cusedarOptions.showOptFields();"/>
             <label class="indent" value="&options.reply.enable.description;"/>
             <spacer class="button-gap"/>
             <grid id="id-reply-options" flex="1">
@@ -179,18 +179,18 @@
 <hbox>
     <hbox id="id-manage" align="start">
         <button id="id-add" class="tool" tooltiptext="&options.add;"
-            oncommand="fidOptions.doAdd();"/>
+            oncommand="cusedarOptions.doAdd();"/>
         <button id="id-remove" class="tool" tooltiptext="&options.remove;"
-            oncommand="fidOptions.doRemove();"/>
+            oncommand="cusedarOptions.doRemove();"/>
         <button id="id-up" class="tool" tooltiptext="&options.up;"
-            oncommand="fidOptions.doUp();"/>
+            oncommand="cusedarOptions.doUp();"/>
         <button id="id-down" class="tool" tooltiptext="&options.down;"
-            oncommand="fidOptions.doDown();"/>
+            oncommand="cusedarOptions.doDown();"/>
     </hbox>
 
     <spacer flex="1"/>
 
-    <button label="&okButton.label;" oncommand="fidOptions.doAccept();"/>
+    <button label="&okButton.label;" oncommand="cusedarOptions.doAccept();"/>
     <button label="&cancelButton.label;" oncommand="window.close();"/>
 </hbox>
 

chrome/content/reply_as_original_recipient.js
@@ -51,7 +51,7 @@ var ReplyAsOriginalRecipient = {
   },
 
   init: function() {
-    /* This will be checked by fid to disable flexible identity replacement when
+    /* This will be checked by cusedar to disable flexible identity replacement when
 	 raor already replaced the From field (when success == true) */
     this.success = false;  /* Initially set to false */
     // Extra Debug (disabled)
@@ -70,11 +70,11 @@ var ReplyAsOriginalRecipient = {
 	let prefs = Components.classes['@mozilla.org/preferences-service;1'].
         getService(Components.interfaces.nsIPrefBranch);
 
-	pref_debug  = prefs.getBoolPref('extensions.fid.debug.console');
-        pref_enable = prefs.getBoolPref('extensions.fid.reply.enable');
-	pref_cc     = prefs.getBoolPref('extensions.fid.reply.checkcc');
-	pref_regexp = prefs.getCharPref('extensions.fid.reply.regexp');
-	pref_sendername = prefs.getCharPref('extensions.fid.reply.sendername');
+	pref_debug  = prefs.getBoolPref('extensions.cusedar.debug.console');
+        pref_enable = prefs.getBoolPref('extensions.cusedar.reply.enable');
+	pref_cc     = prefs.getBoolPref('extensions.cusedar.reply.checkcc');
+	pref_regexp = prefs.getCharPref('extensions.cusedar.reply.regexp');
+	pref_sendername = prefs.getCharPref('extensions.cusedar.reply.sendername');
     } catch (ex) {Components.utils.reportError(ex);}
 
     /* Debug Console Output */
@@ -154,7 +154,7 @@ var ReplyAsOriginalRecipient = {
 	console.log("DEBUG cusedar: Success RE Recipient Isolated = ", originalRecipient);
 
     this.setSender(originalRecipient);   /* Now update the sender */
-    /* This will be checked by fid to disable flexible identity replacement when
+    /* This will be checked by cusedar to disable flexible identity replacement when
 	 raor already replaced the From field (when success == true) */
     this.success = true;  /* Now change it to true, because we did run */
 

commit 3e4c281f4b34e5e89ab2c8788fcfbebdea1d9481

Fix syntax error
introduced with 459a78b25601f1c907ddd8b13dab115c0af1f4c5

chrome/content/helpers.js
@@ -79,4 +79,4 @@ showNotify: function() {
 
 } // cusedarHelpers
 
-window.addEventListener('load', function() cusedarHelpers.onLoad();}, false);
+window.addEventListener('load', function() {cusedarHelpers.onLoad();}, false);